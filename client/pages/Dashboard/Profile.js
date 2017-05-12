c8c71c49baf0c80ad90e3343b01992dcaecdcdc7
ilham farobi <ilhamfarobi@gmail.com>
2017-05-12 14:19:23 +0700

    update profile

--- client/pages/Dashboard/Profile.js
+++ client/pages/Dashboard/Profile.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import config from 'config'
 import Input from 'react-toolbox/lib/input'
 import Dropdown from 'react-toolbox/lib/dropdown'
 import DatePicker from 'react-toolbox/lib/date_picker'
+import Button from 'react-toolbox/lib/button'
 import cookie from 'react-cookie'
 
 import Panel from '../../components/organisms/Panel'
@@ -11,15 +16,67 @@ class Profile extends Component {
     super(props)
     this.state = {
       user: {},
+      password: '',
+      retype: '',
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleChangePassword = this.handleChangePassword.bind(this)
     this.handleChangeGender = this.handleChangeGender.bind(this)
     this.handleChangeProfile = this.handleChangeProfile.bind(this)
     this.handleChangeBirtDate = this.handleChangeBirtDate.bind(this)
+    this.handleSave = this.handleSave.bind(this)
   }
   componentWillMount() {
     const user = cookie.load('user')
+    user.profile.data.birth_date = new Date(user.profile.data.birth_date)
     this.setState({ user })
+  }
+  handleSave(e) {
+    e.preventDefault()
+    const { user, password, retype } = this.state
+    const { token } = this.props
+    const {
+      no_ktp,
+      gender,
+      birth_date,
+      phone_number,
+      profile_photo_url,
+      ktp_photo_url,
+      address,
+      about_me,
+    } = user.profile.data
+    // axios.put(`${config.API_URL}/users/${user.id}`, {
+    //   name: user.name,
+    //   password: password,
+    // }, {
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // })
+    // .then((response) => {
+    //   console.log(response.data)
+    // })
+    const date = new Date(birth_date)
+    axios.put(`${config.API_URL}/user-profiles/${user.id}`, {
+      no_ktp,
+      gender,
+      birth_date: `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`,
+      phone_number,
+      profile_photo_url: 'nanti dari firebase',
+      ktp_photo_url: 'nanti dari firebase',
+      address,
+      about_me,
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      console.log(response)
+    })
+  }
+  handleChangePassword(val, el) {
+    this.setState({ [el.target.name]: val })
   }
   handleChange(val, el) {
     const newUser = Object.assign(this.state.user, {
@@ -94,15 +151,15 @@ class Profile extends Component {
               type="password"
               name="password"
               label="Password"
-              value=""
-              onChange={this.handleChange}
+              value={this.state.password}
+              onChange={this.handleChangePassword}
             />
             <Input
               type="password"
               name="retype"
               label="Retype Password"
-              value=""
-              onChange={this.handleChange}
+              value={this.state.retype}
+              onChange={this.handleChangePassword}
             />
           </form>
         </Panel>
@@ -123,8 +180,8 @@ class Profile extends Component {
                 value: 'Pria',
                 label: 'Pria',
               }, {
-                value: 'Perempuan',
-                label: 'Perempuan',
+                value: 'Wanita',
+                label: 'Wanita',
               }]}
             />
             <DatePicker
@@ -160,6 +217,15 @@ class Profile extends Component {
               value={about_me}
               onChange={this.handleChangeProfile}
             />
+            <Button
+              className={classnames('no-box-shadow', 'no-radius')}
+              primary={true}
+              raised={true}
+              onClick={this.handleSave}
+              label="Save"
+              icon="save"
+              type="submit"
+            />
           </form>
         </Panel>
       </section>
@@ -167,4 +233,8 @@ class Profile extends Component {
   }
 }
 
-export default Profile
+const mapStateToProps = state => ({
+  token: state.auth.token.accessToken,
+})
+
+export default connect(mapStateToProps)(Profile)
