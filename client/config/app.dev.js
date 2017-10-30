17490b04aa1a691bdc8d399db4f8a3b8e69d5ecd
Ilham Farobi <ifr@Ilhams-MacBook-Pro.local>
2017-10-30 21:37:10 +0700

    change config

--- client/config/app.dev.js
+++ client/config/app.dev.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  API_URL: 'http://128.199.231.246:8080/api',
+  API_URL: 'http://139.59.111.7/api',
   GOOGLE_API_KEY: 'AIzaSyBGYp8gDUADooJ14l7UVmKbp7-8D6n7KvI',
-  ROOT_URL: 'http://128.199.231.246:8080',
-  CLIENT_SECRET: '7zIfApF9mkIJf7cYiNdVGYQfzlpNDMY2J4B1tXEc',
+  ROOT_URL: 'http://139.59.111.7',
+  CLIENT_SECRET: '4qLNJxUUzDx1d3qRJkucOz44NsMBi0OhTHhzPOkt',
   FIREBASE_API: 'AIzaSyD09EEQrHVjLGB1HgH5x1sgYFdZQ-YVdvU',
   FIREBASE_STORAGE_BUCKET: 'gs://rumantara-c0a2f.appspot.com/',
   FIREBASE_DATABASE_URL: 'https://rumantara-c0a2f.firebaseio.com/',
